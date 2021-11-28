2e606335d45690e87e435e8f343f86c2a9e22910
Tomoya Teramoto <h840026u@mails.cc.ehime-u.ac.jp>
2021-11-28 15:57:13 +0900

    画像補間の関数で座標指定時に丸めるよう変更

--- program/image_calibration/calibration.py
+++ program/image_calibration/calibration.py
@@ -102,7 +102,7 @@ def calibration_image(frame, projection_func):
         #        |            ...                      ,
         #        v           [輝度mn0, 輝度mn1, 輝度mn2]]]
         #
-        calibrated_frame[(phys_y, phys_x) if grayscale else (phys_y, phys_x, :)] = frame.reshape((height, width) if grayscale else (height, width, 3))
+        calibrated_frame[(np.round(phys_y), np.round(phys_x)) if grayscale else (np.round(phys_y), np.round(phys_x), :)] = frame.reshape((height, width) if grayscale else (height, width, 3))
         return calibrated_frame
 
 # 読み込んだ動画をもとに新しい動画ファイルを作成
