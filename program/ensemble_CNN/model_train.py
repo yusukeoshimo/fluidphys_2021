a91a153d05a8083206142836a1e00a7fec4df601
TomoyaTeramoto <h840026u@mails.cc.ehime-u.ac.jp>
2021-12-09 17:15:23 +0900

    入力が１つと2つのとき両方に対応できるようプログラムを追加，変更

--- program/ensemble_CNN/model_train.py
+++ program/ensemble_CNN/model_train.py
@@ -12,6 +12,35 @@ import gc
 from tensorflow.keras.utils import Sequence
 
 from read_data import read_ymemmap
+from model_operation import check_input_shape
+
+class MySequence(Sequence): # generator
+    def __init__(self, data_size, batch_size, file_name=None, memmap_dir=None, y_dim=None, output_num=None, output_axis=None):
+        # memmap用のファイルのパス
+        X_MEMMAP_PATH = 'x_' + file_name + '.npy'
+        Y_MEMMAP_PATH = 'y_' + file_name + '.npy'
+        self.batch_size = batch_size
+        self.length = math.ceil(data_size / batch_size)
+        self.memmap_X = np.memmap(
+            filename=os.path.join(memmap_dir, X_MEMMAP_PATH), dtype=np.float32, mode='r',
+            shape=(data_size, 32, 32, 2))
+        self.memmap_y = read_ymemmap(filename=os.path.join(memmap_dir, Y_MEMMAP_PATH), dtype=np.float32, mode='r', y_dim=y_dim, output_num=output_num, output_axis=output_axis)
+
+    def __getitem__(self, idx):
+        start_idx = idx * self.batch_size
+        last_idx = start_idx + self.batch_size
+        X = self.memmap_X[start_idx:last_idx]
+        y = self.memmap_y[start_idx:last_idx]
+        
+        return X, y
+
+    def __len__(self):
+        # 全データの長さ
+        return self.length
+
+    def on_epoch_end(self):
+        # epoch終了時の処理
+        pass
 
 class MySequenceF(Sequence): # generator
     def __init__(self, data_size, batch_size, file_name=None, memmap_dir=None, y_dim=None, output_num=None, output_axis=None):
@@ -53,6 +82,8 @@ def model_train(model,
                 output_axis, 
                 data_size
                 ):
+    # モデルの入力形状を確認
+    input_num, input_depth = check_input_shape(model)
     if not load_split_batch:
         #訓練データを外から中に入れる
         x_train_data = np.memmap(filename=os.path.join(memmap_dir, 'x_train_data.npy'), dtype=np.float32, mode='r').reshape(-1, 32, 32, 2)
@@ -60,28 +91,46 @@ def model_train(model,
         x_val_data = np.memmap(filename=os.path.join(memmap_dir, 'x_val_data.npy'), dtype=np.float32, mode='r').reshape(-1, 32, 32, 2)
         y_val_data = read_ymemmap(filename=os.path.join(memmap_dir, 'y_val_data.npy'), y_dim=y_dim, output_num=output_num, output_axis=output_axis)
         
-        history = model.fit([x_train_data[:,:,:,0],x_train_data[:,:,:,1]],
-                            y_train_data,
-                            verbose=verbose,
-                            epochs=epochs,
-                            batch_size=batch_size,
-                            validation_data=([x_val_data[:,:,:,0], x_val_data[:,:,:,1]], y_val_data),
-                            callbacks=callbacks,
-                            )
+        if input_num == 1: # 入力が1の場合
+            history = model.fit(x_train_data,
+                                y_train_data,
+                                verbose=verbose,
+                                epochs=epochs,
+                                batch_size=batch_size,
+                                validation_data=(y_train_data, y_val_data),
+                                callbacks=callbacks,
+                                )
+        elif input_num == 2: # 入力が2の場合
+            history = model.fit([x_train_data[:,:,:,0],x_train_data[:,:,:,1]],
+                                y_train_data,
+                                verbose=verbose,
+                                epochs=epochs,
+                                batch_size=batch_size,
+                                validation_data=([x_val_data[:,:,:,0], x_val_data[:,:,:,1]], y_val_data),
+                                callbacks=callbacks,
+                                )
         del x_train_data, y_train_data, x_val_data, y_val_data
     else:
         #データ数を外から中に入れる
         train_data_size = data_size[0]
         val_data_size = data_size[1]
         
-        # バッチごとにデータの読み込み
-        
-        history = model.fit(x=MySequenceF(train_data_size, batch_size, 'train_data', memmap_dir, y_dim, output_num, output_axis),
-                            verbose=verbose,
-                            epochs=epochs,
-                            callbacks=callbacks,
-                            validation_data=MySequenceF(val_data_size, batch_size, 'val_data', memmap_dir, y_dim, output_num, output_axis),
-                            )
+        if input_num == 1: # 入力が1の場合
+            # バッチごとにデータの読み込み
+            history = model.fit(x=MySequence(train_data_size, batch_size, 'train_data', memmap_dir, y_dim, output_num, output_axis),
+                                verbose=verbose,
+                                epochs=epochs,
+                                callbacks=callbacks,
+                                validation_data=MySequence(val_data_size, batch_size, 'val_data', memmap_dir, y_dim, output_num, output_axis),
+                                )
+        elif input_num == 2: # 入力が2の場合
+            # バッチごとにデータの読み込み
+            history = model.fit(x=MySequenceF(train_data_size, batch_size, 'train_data', memmap_dir, y_dim, output_num, output_axis),
+                                verbose=verbose,
+                                epochs=epochs,
+                                callbacks=callbacks,
+                                validation_data=MySequenceF(val_data_size, batch_size, 'val_data', memmap_dir, y_dim, output_num, output_axis),
+                                )
             
         del train_data_size, val_data_size
     return model, history
